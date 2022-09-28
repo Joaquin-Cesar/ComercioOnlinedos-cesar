d0f36fcaf6ed0e5bb1a288d69bbbd93ba574add4
Joaquin Cesar <joaquincesar777@gmail.com>
2022-09-28 17:08:23 -0300

    firebase II

--- src/components/CartContainers/CartContainers.js
+++ src/components/CartContainers/CartContainers.js
@@ -1,13 +1,43 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { CartContext } from '../../Context/CartContext'
 import "../Estilo/estilo.css"
 import { Link } from 'react-router-dom'
+import { db } from '../../utils/firebise'
+import {collection, addDoc} from "firebase/firestore"
 const CartContainers = () => {
     const {ProductoCartList, removeItem, ClearCart, PrecioTotal}= useContext(CartContext)
+    const [idOrder, setIdOrder] = useState("")
     console.log("ProductoCartList",ProductoCartList.length)
+
+   
+
+    const sendOrder = (evento) =>{
+      evento.preventDefault()
+ 
+      const order ={
+        buyer: {
+          name:evento.target[0].value,
+          phone: evento.target[1].value,
+           email: evento.target[2].value
+        },
+        items: ProductoCartList,
+        total: PrecioTotal()
+      }
+  
+      console.log("order", order)
+
+      const queryRef = collection(db, "orders")
+      addDoc(queryRef, order).then(response=>{
+        setIdOrder(response.id)
+      })
+    }
+
     return (
-   <div> {
-      ProductoCartList.length > 0 ? 
+   <div>
+     {idOrder ? <p>su orden fue creada, id {idOrder}</p>:
+     <>
+     
+    {ProductoCartList.length > 0 ? 
       <div>
       CartContainers
       <div className='CartContainer'>
@@ -25,6 +55,15 @@ const CartContainers = () => {
       </div>
       <button onClick={ClearCart}>Eliminar Carrito</button>
       <p>PrecioTotal: ${PrecioTotal()}</p>
+      <form onSubmit={sendOrder}>
+        <label>Nombre: </label>
+        <input type="text"/>
+        <label>Celular: </label>
+        <input type="text"/>
+        <label>Email: </label>
+        <input type="email"/>
+        <button type='submit'>Enviar orden</button>
+      </form>
   </div> 
   :
   <>
@@ -39,6 +78,9 @@ const CartContainers = () => {
   </div>
   </>
     }
+    </>
+    }
+    
    </div>
   )
 }
